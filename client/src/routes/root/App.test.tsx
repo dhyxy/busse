addf29f75676f71308a504694c22bdb16d90ab44
dhyey <dhyeylalseta@outlook.com>
2023-02-21 18:37:50 -0800

    [test] fixed tests ✅

--- client/src/routes/root/App.test.tsx
+++ client/src/routes/root/App.test.tsx
@@ -1,9 +1,38 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { UserContext } from '../../contexts/user';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const wrapper = BrowserRouter;
+
+test('app renders', () => {
+  render(<App />, { wrapper });
+});
+
+test('app shows login button', () => {
+  render(<App />, { wrapper });
+
+  const loginButton = screen.getByText('login');
+  const logoutButton = screen.queryByText('logout');
+
+  expect(loginButton).toBeVisible();
+  expect(logoutButton).not.toBeInTheDocument();
+});
+
+test('app shows logout button', () => {
+  render(
+    <UserContext.Provider
+      value={{ user: { email: 'test@test.com' }, setUser: (_) => _ }}
+    >
+      <App />
+    </UserContext.Provider>,
+    { wrapper },
+  );
+
+  const logoutButton = screen.getByText('logout');
+  const loginButton = screen.queryByText('login');
+
+  expect(logoutButton).toBeVisible();
+  expect(loginButton).not.toBeInTheDocument();
 });
