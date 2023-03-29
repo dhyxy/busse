ec0543c4606d6f3204da419a4557833b36f3565f
dhyey <dhyeylalseta@outlook.com>
2023-03-28 21:24:23 -0600

    [deploy] fix pdf bug

--- index.ts
+++ index.ts
@@ -18,7 +18,11 @@ const app = express();
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cors());
-app.use(helmet());
+app.use(
+    helmet({
+        contentSecurityPolicy: false,
+    }),
+);
 
 app.disable('etag');
 
